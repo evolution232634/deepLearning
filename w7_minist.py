d2e69d793d2a1236768c22c9b0c8f38cdda23954
evolution232634 <evolution232634@163.com>
2019-01-19 14:04:07 +0800

    第七周作业修改

--- w7_minist.py
+++ w7_minist.py
@@ -6,9 +6,6 @@ mnist = input_data.read_data_sets(data_dir, one_hot=True)
 x = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
 x_image = tf.reshape(x, [-1, 28, 28, 1])
-batch_size = 100
-n_batch = mnist.train.num_examples // batch_size
-learning_rate = tf.Variable(0.01, dtype=tf.float32)
 
 with tf.name_scope('conv1'):
     w_conv1 = tf.Variable(tf.truncated_normal(shape=[5, 5, 1, 32], stddev=0.1),
@@ -42,9 +39,9 @@ with tf.name_scope('fc2'):
     y_out = tf.add(tf.matmul(h_fc1_drop, W_fc2), b_fc2)
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_out))
-#l2_loss = tf.add_n([tf.nn.l2_loss(w) for w in tf.get_collection('WEIGHTS')])
-#total_loss = cross_entropy + 7e-5 * l2_loss
-train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+l2_loss = tf.add_n([tf.nn.l2_loss(w) for w in tf.get_collection('WEIGHTS')])
+total_loss = cross_entropy + 7e-5 * l2_loss
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 sess = tf.Session()
 init_op = tf.global_variables_initializer()
@@ -53,14 +50,15 @@ sess.run(init_op)
 correct_prediction = tf.equal(tf.argmax(y_out, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-for epoch in range(21):
-    sess.run(tf.assign(learning_rate, 0.01 * (0.85 ** epoch)))
-    for step in range(n_batch):
-        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+for step in range(5000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    if step % 100 == 0:
+        train_accuracy = sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+        print("step %d, training accuracy %g" % (step, train_accuracy))
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
 
-    lr = sess.run(learning_rate)
-    acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 0.5})
-    print("test accuracy %g , Learning_rate: %g" % (acc, lr))
+acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 0.5})
+print("test accuracy %g , Learning_rate: %g" % acc)
+
 
 
